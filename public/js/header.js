7269635a04a252c88652386f92324103d6e0c7d8
Martin Faraguna <Martin.Faraguna@ACTIVOCC.LAN>
2024-12-23 16:44:42 -0300

    burger

---
+++ public/js/header.js
@@ -0,0 +1,11 @@
+const list = document.querySelector('#list');
+const abrir = document.querySelector('#abrir');
+const cerrar = document.querySelector('#cerrar');
+
+abrir.addEventListener("click", () => {
+    list.classList.add('visible')
+});
+
+cerrar.addEventListener("click", () => {
+    list.classList.remove('visible')
+});
